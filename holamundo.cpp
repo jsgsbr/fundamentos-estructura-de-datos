3767ee07a86e44b1c26b162c3cfb900a2eeadc8a
Jhon Steven Saavedra <foeblackg@gmail.com>
2021-02-03 10:46:47 -0500

    Edit C++

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-using namespace std;
+#define desayuno_comienza 6
+#define desayuno_termina 9
+#define almuerzo_comienza 12
+#define almuerzo_termina 15
+#define cena_comienza 19
+#define cena_termina 21
 
-int main() {
-    cout << "Hola mundo" << endl;
-    return 0;
-}
+using namespace std;
+ 
+int main()
+{ 
+int horaactual;
+cout<<"que hora es: ";
+cin>>horaactual;
+
+if((desayuno_comienza<=horaactual)&&(horaactual<=desayuno_termina))
+	{
+	cout<<" Es hora de desayunar";
+	}
+else if((almuerzo_comienza<=horaactual)&&(horaactual<=almuerzo_termina))
+	{
+	cout<<" Es hora de almorzar";
+	}
+else if((cena_comienza<=horaactual)&&(horaactual<=cena_termina))
+	{
+	cout<<" Es hora de cenar";
+	}
+else
+	{	
+	cout<<"No es hora de comer";
+	}
+return 0;
+    } 
